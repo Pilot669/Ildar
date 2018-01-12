cde4d47f880fe3c97dac89639f337ba262b9f7fb
Pilot669 <31530783+Pilot669@users.noreply.github.com>
2018-01-12 17:55:03 +0300

    с музыкой

--- app/src/main/java/com/example/pc/ildar/MainActivity.java
+++ app/src/main/java/com/example/pc/ildar/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.pc.ildar;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,14 +11,33 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     private Button  act_change;
-
+//    private MediaPlayer changeSound;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         addListenerOnButton();
+//        imageClic();
+
+//        changeSound = MediaPlayer.create(this, R.raw.bystraya);
+//    }
+
+//    public void imageClic () {
+//        act_change.setOnClickListener(
+//                new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View view) {
+//                        soundPlay(changeSound);
+//
+//                    }
+//                }
+//        );
+//    }
+//    public void soundPlay (MediaPlayer sound){
+//        sound.start();
     }
+
     public void addListenerOnButton () {
         act_change = (Button) findViewById(R.id.change);
         act_change.setOnClickListener(
--- app/src/main/java/com/example/pc/ildar/SecondActivity.java
+++ app/src/main/java/com/example/pc/ildar/SecondActivity.java
@@ -1,35 +1,43 @@
 package com.example.pc.ildar;
 
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+
+
+    //    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        ImageView imageView = new ImageView(this);
+//        imageView.setImageResource(R.drawable.happi);
+//        setContentView(imageView);
+//    }
+//}
+
+
+
+
 
     private ImageView ing1, ing2;
     private TextView text1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        super.OnCreate(savedInstanceState);
+        ImageView(R.drawable.activity_main);
         happi();
     }
-    public void happi () {
 
-        ing1 = (ImageView)findViewById(R.id.happi1);
-        text1 = (TextView)findViewById(R.id.text);
-        ing2 = (ImageView)findViewById(R.id.happi2);
+    public void happi() {
 
+        ing1 = (ImageView) findViewById(R.id.happi1);
+        text1 = (TextView) findViewById(R.id.text);
+        ing2 = (ImageView) findViewById(R.id.happi2);
 
-
-}
-
+    }
 }
